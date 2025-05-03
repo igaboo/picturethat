4b362b03071de15ef2f2141762697bb2ee7b4c0a
igaboo <jadenwatsond@gmail.com>
2025-05-03 03:15:53 -0700

    alternate navigation for sending args

--- lib/utils/navigate.dart
+++ lib/utils/navigate.dart
@@ -6,10 +6,24 @@ void navigate(
   Object? arguments,
 }) {
   final ModalRoute<dynamic>? currentRoute = ModalRoute.of(context);
-
   final String? currentRouteName = currentRoute?.settings.name;
 
-  if (currentRouteName != route) {
-    Navigator.pushNamed(context, route, arguments: arguments);
-  }
+  if (currentRouteName == route) return;
+
+  Navigator.pushNamed(context, route, arguments: arguments);
+}
+
+void navigateRoute(BuildContext context, Widget targetWidget, String route) {
+  final ModalRoute<dynamic>? currentRoute = ModalRoute.of(context);
+  final String? currentRouteName = currentRoute?.settings.name;
+
+  if (currentRouteName == route) return;
+
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => targetWidget,
+      settings: RouteSettings(name: route),
+    ),
+  );
 }
