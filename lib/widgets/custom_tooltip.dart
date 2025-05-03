78c644b8c55e2c8e062bcec1b6f71294ca467b65
igaboo <jadenwatsond@gmail.com>
2025-05-03 03:15:13 -0700

    added fade out animation

--- lib/widgets/custom_tooltip.dart
+++ lib/widgets/custom_tooltip.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:picturethat/utils/preference_utils.dart';
 
+final fadeDuration = const Duration(milliseconds: 300);
+
 class CustomTooltip extends StatefulWidget {
   final String tooltipId;
   final String title;
@@ -19,6 +21,7 @@ class CustomTooltip extends StatefulWidget {
 
 class _CustomTooltipState extends State<CustomTooltip> {
   bool isVisible = false;
+  double opacity = 1.0;
 
   @override
   void initState() {
@@ -28,52 +31,64 @@ class _CustomTooltipState extends State<CustomTooltip> {
 
   void _checkTooltipVisibility() async {
     final bool isTooltipVisible = await getBool(widget.tooltipId);
-    if (!isTooltipVisible) setState(() => isVisible = true);
+    if (!isTooltipVisible) {
+      setState(() => isVisible = true);
+    }
   }
 
   void _hideTooltip() {
-    setState(() => isVisible = false);
-    setBool(widget.tooltipId, true);
+    setState(() => opacity = 0.0);
+    Future.delayed(fadeDuration, () {
+      setState(() => isVisible = false);
+      setBool(widget.tooltipId, true);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
         visible: isVisible,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surfaceContainer,
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Theme.of(context).colorScheme.surfaceContainerHighest,
-              ),
-            ),
+        child: SafeArea(
+          child: AnimatedOpacity(
+            duration: fadeDuration,
+            opacity: opacity,
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12.0,
-                horizontal: 16.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(widget.title,
-                      style: Theme.of(context).textTheme.titleMedium),
-                  Text(widget.message),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: TextButton(
-                      onPressed: _hideTooltip,
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.all(5.0),
-                        minimumSize: Size.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      ),
-                      child: const Text("Got it"),
-                    ),
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceContainer,
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(
+                    color:
+                        Theme.of(context).colorScheme.surfaceContainerHighest,
                   ),
-                ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12.0,
+                    horizontal: 16.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.title,
+                          style: Theme.of(context).textTheme.titleMedium),
+                      Text(widget.message),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: TextButton(
+                          onPressed: _hideTooltip,
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.all(5.0),
+                            minimumSize: Size.zero,
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          child: const Text("Got it"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
