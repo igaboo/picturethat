422b6181e57fa852323246a7e25efbe22cc1cbd4
igaboo <jadenwatsond@gmail.com>
2025-05-04 00:19:15 -0700

    simple rename

--- lib/utils/show_snackbar.dart
+++ lib/utils/show_snackbar.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:picture_that/utils/get_error_message.dart';
 
-void handleError(BuildContext context, dynamic e) {
+void customShowSnackbar(BuildContext context, dynamic e) {
   String errorMessage;
 
   if (e is FirebaseAuthException) {
